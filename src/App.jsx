4852be3fc35ed9fee9a5ed0ce82ebe91ee043651
enguillem <enguillem@gmail.com>
2023-08-23 07:09:11 +0200

    configurat fins tilewind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from "react"
+// import React from "react"
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import TaskForm from "./pages/TaskForm"
 function App() {
 
   return (
-    <div>hellow wordfsdfld</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/task/new" element={<TaskForm/>} />
+      </Routes>
+    </BrowserRouter>
+    )
 }
 
 export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+function HomePage(){
+    return (
+        <h1 className="text-3xl font-bold text-red-500">HomePage</h1>
+    )
+}
+
+export default HomePage
