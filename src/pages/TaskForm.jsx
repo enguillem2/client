bb5082e22729d5a02e31d6ef03b306e3bc896f6e
enguillem <enguillem@gmail.com>
2023-08-23 08:16:38 +0200

    petició feta amb axios

--- src/pages/TaskForm.jsx
+++ src/pages/TaskForm.jsx
@@ -1,34 +1,42 @@
 
-import { useState } from "react" 
+import { useState } from "react"
+import axios from "axios"
+
 function TaskForm() {
-  const [title,setTitle]=useState('')
-  const [description,setDescription] =useState('')
-  const handleSubmit= async(e)=>{
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    let url="http://192.168.1.156:8008/api/tasks"
-    const res = await fetch(url,{
-      method:"POST",
-      body: JSON.stringify({
-        title,
-        description
-      }),
-      headers:{
-        'Content-type':'application/json'
-      }
+    let url = "http://192.168.1.156:8008/api/tasks"
+    // const res = await fetch(url,{
+    //   method:"POST",
+    //   body: JSON.stringify({
+    //     title,
+    //     description
+    //   }),
+    //   headers:{
+    //     'Content-type':'application/json'
+    //   }
+    // })
+
+    //amb axios
+    const res = await axios.post(url, {
+      title,
+      description
     })
-    const dato= await res.json()
-    console.log(dato)
+    console.log(res)
+    e.target.reset()
   }
   return (
     <div className="flex items-center justify-center h-[calc(100vh-10rem)]">
       <form className="bg-zinc-950 p-10" onSubmit={handleSubmit} >
         <input type="text" placeholder="title"
           className="block py-2 px-3 mb-4 w-full text-black"
-          onChange={(e)=>setTitle(e.target.value)}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <textarea placeholder="description" rows="3"
           className="block py-2 px-3 mb-4 w-full text-black"
-          onChange={(e)=>setDescription(e.target.value)}
+          onChange={(e) => setDescription(e.target.value)}
 
         ></textarea>
         <button>Save</button>
