a52c59243459480b15b4143c8837a44049277cc6
enguillem <enguillem@gmail.com>
2023-08-25 06:50:25 +0200

    canvis però encara no va bé

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,48 @@
-function HomePage(){
+import { useEffect, useState,useCallback } from "react";
+import TaskList from "../components/TaskList";
+import { fetchTasks } from "../api/tasks";
+import axios from "axios"
+
+
+function HomePage() {
+    const [tasks, setTasks] = useState([]);
+    const url="http://192.168.1.156:8008/api/tasks"
+
+    const fetchTasks =useCallback(async()=>{
+        const res = await axios.get(url);
+        console.log("res::",res.data)
+        setTasks(res.data)
+        console.log("tasks:",tasks)
+    },[]);
+    console.log("tasks2:",tasks)
+
+
+
+
+    // useEffect(() => {
+    //     async function fetchTasks() {
+    //         const res = await axios.get(url)
+    //         console.log("res",res.data)
+    //         setTasks(res.data)
+    //         console.log("taks",tasks)
+
+    //     }
+    //     fetchTasks()
+    // }, []);
+
     return (
-        <h1 className="text-3xl font-bold text-red-500">HomePage</h1>
-    )
+        <>
+            <h1>Tasks</h1>
+            <button onClick={fetchTasks}>search</button>
+            <div>
+                <ul>
+                {tasks.map((task)=>{
+                    <li>{task.title}</li>
+                })}
+                </ul>
+            </div>
+        </>
+    );
 }
 
-export default HomePage
+export default HomePage;
