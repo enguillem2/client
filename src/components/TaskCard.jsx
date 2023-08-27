a94520953e2e0cddec2298823287c0337e04aa09
enguillem <enguillem@gmail.com>
2023-08-27 06:53:06 +0200

    projecte acabat

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -19,10 +19,15 @@ function TaskCard({ task }) {
             e.stopPropagation()
             const res= await updateTask(task._id,{completed: !task.completed})
             console.log("change completed",res)
+            if(res.status==200){
+
+              window.location.reload();
+            }
           }}
         >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-          strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          strokeWidth="1.5" stroke="currentColor" 
+          className={`w-6 h-6 ${task.completed ? 'text-green-500':''}`}>
             <path 
             strokeLinecap="round" 
             strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
